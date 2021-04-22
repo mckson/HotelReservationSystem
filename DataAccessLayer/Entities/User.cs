45912b1ab6a47bf349b2f75a57daf61d1e52125f
Dementiy <maxim.silov.2016@gmail.com>
2021-04-22 11:56:59 +0300

    Add User entity
    User entity describes account to log in system

---
+++ DataAccessLayer/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace DataAccessLayer.Entities
+{
+    class User
+    {
+        //Primary Key
+        public int Id { get; set; }
+        //Foreign Key
+        public int RoleId { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; } //delete later
+        //public string Email { get; set; }
+    }
+}
