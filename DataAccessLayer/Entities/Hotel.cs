084172cfcf8f5634332df40d17d7f88b9591d274
Dementiy <maxim.silov.2016@gmail.com>
2021-04-22 11:46:24 +0300

    Add Hotel entity Location property
    Add Location property instead of Country, City etc. properties

--- DataAccessLayer/Entities/Hotel.cs
+++ DataAccessLayer/Entities/Hotel.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DataAccessLayer.Entities
+﻿namespace DataAccessLayer.Entities
 {
     class Hotel
     {
+        //Primary Key
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Country { get; set; }
-        public string City { get; set; }
+        //Foreign Key
+        public int LocationId { get; set; }
         public int RoomsNumber { get; set; }
         //public List<Room> Rooms { get; set; }
     }
