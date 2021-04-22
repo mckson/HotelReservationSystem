b0fabea8109f82d53fc7692518668d19e4b1ef79
Dementiy <maxim.silov.2016@gmail.com>
2021-04-22 12:01:40 +0300

    Add Role entity
    Role entity has Name and PrmissionsId as foreign key

---
+++ DataAccessLayer/Entities/Role.cs
@@ -0,0 +1,11 @@
+﻿namespace DataAccessLayer.Entities
+{
+    class Role
+    {
+        //Primary Key
+        public int Id { get; set; }
+        public string RoleName { get; set; }
+        //Foreign Key
+        public int PermissionsId { get; set; }
+    }
+}
