4820eaeaf4effb4f3c726451324d292b0874dda1
Dementiy <maxim.silov.2016@gmail.com>
2021-04-22 10:56:46 +0300

    Add Room entity
    Add Room.cs file in DataAccessLayer/Entities. It contains initial descrtption of a Room from a Hotel

---
+++ DataAccessLayer/Entities/Room.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Entities
+{
+    class Room
+    {
+        public int Id { get; set; }
+        public int HotelId { get; set; }
+        public int RoomNumber { get; set; }
+        public int FloorNumber { get; set; }
+        //public RoomType RoomType { get; set; }
+        public int SeatNumber { get; set; } //quantity of мест (rename later) 
+    }
+}
