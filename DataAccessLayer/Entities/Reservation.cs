65bca68bcd85821db32d2bb209c48a59b8755798
Dementiy <maxim.silov.2016@gmail.com>
2021-04-22 11:39:23 +0300

    Add Reservation entity
    Add Reservation.cs in DataAccessLayer/Entities. It contains initial Reservation entity description

---
+++ DataAccessLayer/Entities/Reservation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DataAccessLayer.Entities
+{
+    class Reservation
+    {
+        //Primary Key
+        public int Id { get; set; }
+        //Foreign Key
+        public int GuestId { get; set; }
+        //Foreign Key
+        public int RoomId { get; set; }
+        //public List<Room> Rooms { get; set; }
+        public DateTime DateIn { get; set; }
+        public DateTime DateOut { get; set; }
+        public int TotalDays { get; set; }
+        //public int TotalRooms { get; set; }
+        public decimal Deposit { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
