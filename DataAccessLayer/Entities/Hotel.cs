83399c12b65ac6a619131fbce00046eedb153fcd
Dementiy <maxim.silov.2016@gmail.com>
2021-04-22 11:34:21 +0300

    Fix Hotel empty entity
    Add properties to Hotel entity that were skipped in 'Add Hotel entity' commit

--- DataAccessLayer/Entities/Hotel.cs
+++ DataAccessLayer/Entities/Hotel.cs
@@ -6,5 +6,11 @@ namespace DataAccessLayer.Entities
 {
     class Hotel
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public int RoomsNumber { get; set; }
+        //public List<Room> Rooms { get; set; }
     }
 }
