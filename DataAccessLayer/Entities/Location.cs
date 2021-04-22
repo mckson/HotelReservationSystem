5ca43694edc6b3c09c96038bb95e6d56b948656e
Dementiy <maxim.silov.2016@gmail.com>
2021-04-22 11:43:39 +0300

    Add Location entity
    Location.cs added in DataAccessLayer/Entities. It contains initial structure of Location entity: country, region, city, street and building number

---
+++ DataAccessLayer/Entities/Location.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Entities
+{
+    class Location
+    {
+        //Primary Key
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public string Region { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public int BuildingNumber { get; set; }          
+    }
+}
